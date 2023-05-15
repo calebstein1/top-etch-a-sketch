16c5a0621146f7fc34fe610298b110ecde569a1f
Caleb Stein <caleb@calebstein.net>
2023-05-15 11:52:45 -0700

    added redraw form function

--- sketch.js
+++ sketch.js
@@ -1,17 +1,24 @@
 const etch = document.querySelector(".etch-container");
+const newBoard = document.querySelector("form");
 let rows = 16;
 
 const drawBoard = (dim) => {
-    const cellDim = 960 / rows;
+    etch.innerHTML = "";
+    const cellDim = 960 / dim;
     const newRow = document.createElement("div");
     const newCell = document.createElement("div");
     newRow.setAttribute("class", "etch-row");
     newCell.setAttribute("class", "etch-cell");
-    for (let i = 0; i < rows; i++)
+    for (let i = 0; i < dim; i++)
         etch.appendChild(newRow.cloneNode(true)); 
-    for (let i = 0; i < rows; i++)
+    for (let i = 0; i < dim; i++)
         document.querySelectorAll(".etch-row").forEach(element => element.appendChild(newCell.cloneNode(true)));
     document.querySelectorAll(".etch-cell").forEach(element => element.setAttribute("style", `width: ${cellDim}px; height: ${cellDim}px`));
 }
+
+newBoard.addEventListener("submit", event => {
+    event.preventDefault();
+    drawBoard(document.querySelector("input[type=number]").value);
+})
 
 window.addEventListener("load", drawBoard(rows));
